Fix time embedding projection and use broadcasting for addition to fused tensor in the right dimensions

guided-diffusion/time_embedding.py
@@ -4,17 +4,41 @@ from torch import nn
 
 class TimeEmbedding(nn.Module):
     """
-    Input: t [B]
-    Output: t_embedd [B, F]
-    This module is used to embedd the time t into a vector of size F.
-    The embedding is done by using the sine and cosine functions.
+    TimeEmbedding module for generating time-based embeddings.
+
+    Args:
+        dim (int): The output dimension of the time embeddings. It should be an even number.
+
+    Example:
+        >>> time_embedding = TimeEmbedding(dim=128)
+        >>> inputs = torch.tensor([0, 1, 2, 3, 4])  # Assuming inputs represent time steps
+        >>> embeddings = time_embedding(inputs)
     """
     
-    def __init__(self, dim): # set output dim F as an EVEN number!
+    def __init__(self, dim):
         super().__init__()
         self.dim = dim
 
-    def forward(self, x): # x needs to be of type torch.tensor with length N!
+    def forward(self, x):
+        """
+        Generate time-based embeddings.
+
+        Args:
+            x (torch.Tensor): Input tensor representing time steps. It should be of shape (B,).
+
+        Returns:
+            torch.Tensor: Time embeddings of shape (B, dim).
+
+        Note:
+            - The input tensor `x` needs to be of type torch.Tensor with length B.
+            - The output tensor will have shape (B, dim) where dim is the specified output dimension.
+            - The specified output dimension `dim` should be an even number.
+
+        Example:
+            >>> time_embedding = TimeEmbedding(dim=128)
+            >>> inputs = torch.tensor([0, 1, 2, 3, 4])  # Assuming inputs represent time steps
+            >>> embeddings = time_embedding(inputs)
+        """
         device = x.device
         half_dim = self.dim // 2
         emb = math.log(10000) / (half_dim - 1)