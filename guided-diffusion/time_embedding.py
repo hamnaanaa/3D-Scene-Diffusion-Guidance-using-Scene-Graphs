0d7c806a5ea715e76a7498708739f6eebd2286fd
kasothaphie <kasothaphie@gmail.com>
2023-06-21 13:17:37 +0200

    Add option for onehot enc of labels in X

--- guided-diffusion/time_embedding.py
+++ guided-diffusion/time_embedding.py
@@ -65,8 +65,9 @@ class TimeEmbedding(nn.Module):
         device = x.device
 
         half_dim = self.dim // 2
+        n = 10000
 
-        emb = math.log(10000) / (half_dim - 1)
+        emb = math.log(n) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, device=device, dtype=torch.float32) * -emb)
         emb = x[:, None] * emb[None, :]
 
